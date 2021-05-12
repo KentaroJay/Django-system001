ac0852931d3fa05b66ac033da22001756285c28b
MacBook Pro <macbookpro@takahashikentaroutaino-MacBook-Pro.local>
2021-05-12 17:12:55 +0900

    finished chapter 2

--- app/hello/views.py
+++ app/hello/views.py
@@ -1,17 +1,28 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import HelloForm
 
 
-def index(request):
-    params = {
-        'title': 'Hello',
-        'message': 'your data:',
-        'form': HelloForm()
-    }
-    if (request.method == 'POST'):
-        params['message'] = '名前：' + request.POST['name'] + \
-            '<br>メール：' + request.POST['mail'] + \
-            '<br>年齢：' + request.POST['age']
-        params['form'] = HelloForm(request.POST)
-    return render(request, 'hello/index.html', params)
+class HelloView(TemplateView):
+
+    def __init__(self):
+        self.params = {
+            'title': 'Hello',
+            'form': HelloForm(),
+            'result': None
+        }
+
+    def get(self, request):
+        return render(request, 'hello/index.html', self.params)
+
+
+    def post(self, request):
+        ch = request.POST.getlist('choice')
+        result = '<ol class="list-group"><b>selected:</b>'
+        for item in ch:
+            result += '<li class="list-group-item">' + item + '</li>'
+        result += '</ol>'
+        self.params['result'] = result
+        self.params['form'] = HelloForm(request.POST)
+        return render(request, 'hello/index.html', self.params)
